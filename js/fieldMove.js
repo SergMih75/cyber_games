948ffb7c2abea500b5a3e666490ca8e1322120c5
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2024-10-26 12:50:59 +0300

    js add

---
+++ js/fieldMove.js
@@ -0,0 +1,19 @@
+﻿let fieldJs = document.querySelectorAll('.field-js')
+
+document.addEventListener('scroll', () => {
+	fieldJs.forEach(item => {
+		if (
+			item.getBoundingClientRect().top < innerHeight / 2 &&
+			item.getBoundingClientRect().top > 0 &&
+			!item.classList.contains('field-js-move')
+		) {
+			item.classList.add('field-js-move')
+		}
+		if (
+			item.getBoundingClientRect().top > innerHeight + 5 &&
+			item.classList.contains('field-js-move')
+		) {
+			item.classList.remove('field-js-move')
+		}
+	})
+})
